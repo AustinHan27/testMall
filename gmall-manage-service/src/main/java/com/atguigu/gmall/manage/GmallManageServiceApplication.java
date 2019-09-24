97b5fe061d5f65ece9a7b43d6a92f4d544dbcba3
hyysy04 <79199541@qq.com>
2019-09-24 11:27:25 +0800

    day1

---
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/GmallManageServiceApplication.java
@@ -0,0 +1,19 @@
+package com.atguigu.gmall.manage;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.atguigu.gmall.manage.mapper")
+@EnableTransactionManagement
+@ComponentScan(basePackages = "com.atguigu.gmall")
+public class GmallManageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallManageServiceApplication.class, args);
+    }
+
+}
